267f0b7427868d286cc316310f985a1411833e50
Danyal Faheem <danyal.faheem@arbisoft.com>
2023-10-25 15:27:31 +0500

    Updated settings

--- task1/apps.py
+++ task1/apps.py
@@ -4,8 +4,10 @@ task1 Django application initialization.
 
 from django.apps import AppConfig
 from edx_django_utils.plugins.constants import (
-    PluginURLs, PluginSettings, PluginContexts
+    PluginURLs
 )
+
+from openedx.core.djangoapps.plugins.constants import ProjectType
 
 class Task1Config(AppConfig):
     """
@@ -15,33 +17,11 @@ class Task1Config(AppConfig):
     name = 'task1'
     
     plugin_app = {
-        'url_config': {
-            'lms.djangoapp': {
-                'namespace': 'courses',
-                'regex': '^api/courses/',
-                'relative_path': 'api.urls',
-            }
+        PluginURLs.CONFIG: {
+            ProjectType.LMS: {
+               PluginURLs.NAMESPACE: 'task1',
+                PluginURLs.REGEX: r'^api/task1/',
+                PluginURLs.RELATIVE_PATH: 'urls',
+            },
         },
-        'settings_config': {
-            'lms.djangoapp': {
-                'production': { 'relative_path': 'settings.production' },
-                'common': { 'relative_path': 'settings.common' },
-            }
-        },
-        'signals_config': {
-            'lms.djangoapp': {
-                'relative_path': 'my_signals',
-                'receivers': [{
-                    'receiver_func_name': 'on_signal_x',
-                    'signal_path': 'full_path_to_signal_x_module.SignalX',
-                    'dispatch_uid': 'courses.my_signals.on_signal_x',
-                    'sender_path': 'full_path_to_sender_app.ModelZ',
-                }],
-            }
-        },
-        'view_context_config': {
-            'lms.djangoapp': {
-                'course_dashboard': 'courses.context_api.get_dashboard_context'
-            }
-        }
     }
--- test_settings.py
+++ test_settings.py
@@ -59,3 +59,5 @@ TEMPLATES = [{
         ],
     },
 }]
+
+DEBUG = True
