5a94fa93a566aaa7837fda124531c47fb7888b6c
Danyal Faheem <danyal.faheem@arbisoft.com>
2023-10-26 10:34:14 +0500

    Updated URL

--- task1/urls.py
+++ task1/urls.py
@@ -5,5 +5,5 @@ from django.urls import path
 from task1.views import CourseListAPIView
 
 urlpatterns = [
-    path('v1/courses/', CourseListAPIView.as_view(), name='course_list')
+    path('list-filter/', CourseListAPIView.as_view(), name='course_list_filter')
 ]
